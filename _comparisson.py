c36f282b732508599c585d9a6d3b82350c42472e
neret22 <36138471+neret22@users.noreply.github.com>
2021-10-27 10:57:51 +0200

    Update _comparisson.py

--- _comparisson.py
+++ _comparisson.py
@@ -33,9 +33,9 @@ cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:,-1].values
 
-#splitting the dataset to train and test sets
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+
 
 #Naive Bayes Classification
 
@@ -117,3 +117,67 @@ classifier_forest.fit(X_train_scaled, y_train)
 #Predicting the Test set results
 y_pred_forest = classifier_forest.predict(X_test_scaled)
 
+#splitting the dataset to train and test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+
+#Calculate confusion matrix for all classifications
+
+cm_naive = confusion_matrix(y_test, y_pred_naive)
+print(cm_naive)
+cm_logistic = confusion_matrix(y_test, y_pred_logistic)
+print(cm_logistic)
+cm_k = confusion_matrix(y_test, y_pred_k)
+print(cm_k)
+cm_svc = confusion_matrix(y_test, y_pred_svc)
+print(cm_svc)
+cm_svm = confusion_matrix(y_test, y_pred_svm)
+print(cm_svm)
+cm_tree = confusion_matrix(y_test, y_pred_tree)
+print(cm_tree)
+cm_forest = confusion_matrix(y_test, y_pred_forest)
+print(cm_forest)
+
+
+#calculate accuracy score for all classifications
+
+print("accuracy_score_naive is",  round(accuracy_score(y_test, y_pred_naive), 5))
+print("accuracy_score_logistic is",  round(accuracy_score(y_test, y_pred_logistic), 5))
+print("accuracy_score_k is",  round(accuracy_score(y_test, y_pred_k), 5))
+print("accuracy_score_svc is",  round(accuracy_score(y_test, y_pred_svc), 5))
+print("accuracy_score_svm is",  round(accuracy_score(y_test, y_pred_svm), 5))
+print("accuracy_score_tree is",  round(accuracy_score(y_test, y_pred_tree), 5))
+print("accuracy_score_forest is",  round(accuracy_score(y_test, y_pred_forest), 5))
+
+
+#calculate precision score for all classiffications
+
+print("precision_score_naive is",  round(precision_score(y_test, y_pred_naive,  average='binary'), 5))
+print("precision_score_logistic is",  round(precision_score(y_test, y_pred_logistic,  average='binary'), 5)) 
+print("precision_score_k is",  round(precision_score(y_test, y_pred_k,  average='binary'), 5)) 
+print("precision_score_svc is",  round(precision_score(y_test, y_pred_svc,  average='binary'), 5)) 
+print("precision_score_svm is",  round(precision_score(y_test, y_pred_svm,  average='binary'), 5))
+print("precision_score_tree is",  round(precision_score(y_test, y_pred_tree,  average='binary'), 5)) 
+print("precision_score_forest is",  round(precision_score(y_test, y_pred_forest,  average='binary'), 5)) 
+
+#calculate recall score for all classifications
+
+print("recall_score_naive is", round(recall_score(y_test, y_pred_naive, average='binary'), 5)) 
+print("recall_score_logistic is", round(recall_score(y_test, y_pred_logistic, average='binary'), 5))
+print("recall_score_k is", round(recall_score(y_test, y_pred_k, average='binary'), 5))
+print("recall_score_svc is", round(recall_score(y_test, y_pred_svc, average='binary'), 5))
+print("recall_score_svm is", round(recall_score(y_test, y_pred_svm, average='binary'), 5))  
+print("recall_score_tree is", round(recall_score(y_test, y_pred_tree, average='binary'), 5)) 
+print("recall_score_forest is", round(recall_score(y_test, y_pred_forest, average='binary'), 5)) 
+
+#calculate f-score for all classifications
+
+print("f1_score_naive is", round(f1_score(y_test, y_pred_naive, average='binary'), 5)) 
+print("f1_score_logistic is", round(f1_score(y_test, y_pred_logistic, average='binary'), 5)) 
+print("f1_score_k is", round(f1_score(y_test, y_pred_k, average='binary'), 5))
+print("f1_score_svc is", round(f1_score(y_test, y_pred_svc, average='binary'), 5))
+print("f1_score_svm is", round(f1_score(y_test, y_pred_svm, average='binary'), 5)) 
+print("f1_score_tree is", round(f1_score(y_test, y_pred_tree, average='binary'), 5))
+print("f1_score_forest is", round(f1_score(y_test, y_pred_forest, average='binary'), 5))
