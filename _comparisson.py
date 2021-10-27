2978d0032edc625e40491058c89b8f9a6e727fcd
neret22 <36138471+neret22@users.noreply.github.com>
2021-10-27 11:00:55 +0200

    Update _comparisson.py

--- _comparisson.py
+++ _comparisson.py
@@ -9,7 +9,6 @@ from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, prec
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
 
 #cleaning text for further manipulations
-#here we 
 import re
 import nltk
 nltk.download('stopwords')
@@ -26,18 +25,19 @@ for i in range (0, 1000):
   review = [ps.stem(word) for word in review if not word is set(all_stopwords)]
   review = ' '.join(review)
   corpus.append(review)
-  
-#creating the Bag of Words model 
+
+############################# 
+#creating the Bag of Words model
+#############################
+
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:,-1].values
 
-
-
-
-
+#############################
 #Naive Bayes Classification
+#############################
 
 #Training the Naive Bayes model on the Training set
 from sklearn.naive_bayes import GaussianNB
@@ -47,8 +47,10 @@ classifier_naive.fit(X_train, y_train)
 #Predicting the Test set results
 y_pred_naive = classifier_naive.predict(X_test)
 
-
+#############################
 #Logistic Regression
+#############################
+
 #Scaling the training and test sets
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
@@ -64,7 +66,9 @@ classifier_logistic.fit(X_train_scaled, y_train)
 y_pred_logistic = classifier_logistic.predict(X_test_scaled)
 
 
+#############################
 #K-Nearest Neighbors
+#############################
 
 #Training the K-Nearest Neighbors model on the Training set
 from sklearn.neighbors import KNeighborsClassifier
@@ -75,7 +79,10 @@ classifier_k.fit(X_train_scaled, y_train)
 y_pred_k = classifier_k.predict(X_test_scaled)
 
 
+#############################
 #Support Vector Machine
+#############################
+
 #Training the Support Vector Machine model on the Training set
 from sklearn.svm import SVC
 classifier_svc = SVC( kernel = 'linear', random_state = 0)
@@ -85,7 +92,9 @@ classifier_svc.fit(X_train_scaled, y_train)
 y_pred_svc = classifier_svc.predict(X_test_scaled)
 
 
+#############################
 #Kernel SVM
+#############################
 
 #Training the Kernel SVM model on the Training set
 from sklearn.svm import SVC
@@ -96,7 +105,9 @@ classifier_svm.fit(X_train_scaled, y_train)
 y_pred_svm = classifier_svm.predict(X_test_scaled)
 
 
+#############################
 #Decision Tree Classification
+#############################
 
 #Training the Decision Tree Classification model on the Training set
 from sklearn.tree import DecisionTreeClassifier
@@ -107,7 +118,9 @@ classifier_tree.fit(X_train_scaled, y_train)
 y_pred_tree = classifier_tree.predict(X_test_scaled)
 
 
+#############################
 #Random Forest Classification
+#############################
 
 #Training the Random Forest Classification model on the Training set
 from sklearn.ensemble import RandomForestClassifier
@@ -122,8 +135,9 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 
-
+#############################
 #Calculate confusion matrix for all classifications
+#############################
 
 cm_naive = confusion_matrix(y_test, y_pred_naive)
 print(cm_naive)
@@ -141,7 +155,9 @@ cm_forest = confusion_matrix(y_test, y_pred_forest)
 print(cm_forest)
 
 
+#############################
 #calculate accuracy score for all classifications
+#############################
 
 print("accuracy_score_naive is",  round(accuracy_score(y_test, y_pred_naive), 5))
 print("accuracy_score_logistic is",  round(accuracy_score(y_test, y_pred_logistic), 5))
@@ -152,7 +168,9 @@ print("accuracy_score_tree is",  round(accuracy_score(y_test, y_pred_tree), 5))
 print("accuracy_score_forest is",  round(accuracy_score(y_test, y_pred_forest), 5))
 
 
+#############################
 #calculate precision score for all classiffications
+#############################
 
 print("precision_score_naive is",  round(precision_score(y_test, y_pred_naive,  average='binary'), 5))
 print("precision_score_logistic is",  round(precision_score(y_test, y_pred_logistic,  average='binary'), 5)) 
@@ -162,7 +180,10 @@ print("precision_score_svm is",  round(precision_score(y_test, y_pred_svm,  aver
 print("precision_score_tree is",  round(precision_score(y_test, y_pred_tree,  average='binary'), 5)) 
 print("precision_score_forest is",  round(precision_score(y_test, y_pred_forest,  average='binary'), 5)) 
 
+
+#############################
 #calculate recall score for all classifications
+#############################
 
 print("recall_score_naive is", round(recall_score(y_test, y_pred_naive, average='binary'), 5)) 
 print("recall_score_logistic is", round(recall_score(y_test, y_pred_logistic, average='binary'), 5))
@@ -172,7 +193,10 @@ print("recall_score_svm is", round(recall_score(y_test, y_pred_svm, average='bin
 print("recall_score_tree is", round(recall_score(y_test, y_pred_tree, average='binary'), 5)) 
 print("recall_score_forest is", round(recall_score(y_test, y_pred_forest, average='binary'), 5)) 
 
+
+#############################
 #calculate f-score for all classifications
+#############################
 
 print("f1_score_naive is", round(f1_score(y_test, y_pred_naive, average='binary'), 5)) 
 print("f1_score_logistic is", round(f1_score(y_test, y_pred_logistic, average='binary'), 5)) 
